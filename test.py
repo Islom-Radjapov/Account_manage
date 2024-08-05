01fbd1444756c5304d5432f30954b3275845ccea
Islom Radjapov <radjapov_islom@mail.ru>
2024-08-05 07:49:36 +0500

    open driver

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
-url1 = "https://kortanafx.com/"
+
 # URL of website
 url = "https://global.mytaurex.com/en/account-opening-process?fl=0"
 login = "siwanac556@qodiq.com"
@@ -16,28 +16,36 @@ driver = webdriver.Chrome()
 #driver.set_window_size(800, 600)
 driver.get(url)
 
-driver.implicitly_wait(10)
-#WebDriverWait(driver, 10).until(EC.find_element_by_id(By.XPATH, "//input[@id='userEmail']"))
+# waiting
+WebDriverWait(driver, 10)
 
-#username = driver.find_element_by_name("userEmail")#.send_keys(login))
+# login kiritish
 driver.find_element(by=By.XPATH, value="//input[@id='userEmail'][@class='form-control userEmail']").click()
 driver.find_element(by=By.XPATH, value="//input[@id='userEmail'][@class='form-control userEmail']").send_keys(login)
-
+# parol kiritish
 driver.find_element(by=By.XPATH, value="//input[@id='userPassword'][@class='form-control userPassword']").click()
 driver.find_element(by=By.XPATH, value="//input[@id='userPassword'][@class='form-control userPassword']").send_keys(pasword)
+# login qib kirish knopkasini boish
+driver.find_element(by=By.XPATH, value="//button[@id='login_btn'][@class='btn btn-theme-primary btn-loader']").click()
+
+try:
+    # waiting
+    WebDriverWait(driver, 10)
+    # Get started oynasini olib tashlash uchun start knopkasini bosish
+    driver.find_element(by=By.XPATH, value="//button[@type='button'][@class='btn btn-default btn-close']").click()
+except:
+    pass
+try:
+    # waiting
+    WebDriverWait(driver, 10)
+    # Get started oynasini olib tashlash uchun start knopkasini bosish
+    driver.find_element(by=By.XPATH, value="//button[@type='button'][@class='btn btn-theme-secondary']").click()
+except:
+    pass
+
+# waiting
+WebDriverWait(driver, 10)
 
 
-#username = driver.find_element(by=By.CLASS_NAME, value="btn btn-primary")
-#print(username)
-#a = driver.find_element(by=By.CLASS_NAME, value="btn btn-primary")
-
-
-sleep(3)
-# getting the button by class name
-#button = driver.find_element(by='id', value=id)
-
-# clicking on the button
-#button.click()
-
-
-
+sleep(10)
+#driver.refresh()
