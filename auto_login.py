6b4ed6378b89187deb0c47277da5bf151970be48
Islom Radjapov <radjapov_islom@mail.ru>
2024-08-09 00:17:12 +0500

    finsh project

--- auto_login.py
+++ auto_login.py
@@ -2,74 +2,62 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from time import sleep
+from selenium.webdriver.support import expected_conditions as EC
 
-# URL of website
-url = "https://global.mytaurex.com/en/account-opening-process?fl=0"
-login = "siwanac556@qodiq.com"
-pasword = "Asd_1234"
+def Change_password(email, pasword, old_password, new_password):
+    driver = webdriver.Chrome()
+    driver.set_window_size(1550, 800)
+    driver.get("https://global.mytaurex.com/en/account-opening-process?fl=0")
 
-
-# Here Chrome  will be used
-
-driver = webdriver.Chrome()
-driver.set_window_size(1550, 800)
-driver.get(url)
-
-# waiting
-WebDriverWait(driver, 10)
-
-# login kiritish
-driver.find_element(by=By.XPATH, value="//input[@id='userEmail'][@class='form-control userEmail']").click()
-driver.find_element(by=By.XPATH, value="//input[@id='userEmail'][@class='form-control userEmail']").send_keys(login)
-# parol kiritish
-driver.find_element(by=By.XPATH, value="//input[@id='userPassword'][@class='form-control userPassword']").click()
-driver.find_element(by=By.XPATH, value="//input[@id='userPassword'][@class='form-control userPassword']").send_keys(pasword)
-# login qib kirish knopkasini boish
-driver.find_element(by=By.XPATH, value="//button[@id='login_btn'][@class='btn btn-theme-primary btn-loader']").click()
-sleep(10)
-try:
     # waiting
-    WebDriverWait(driver, 10)
-    # Get started oynasini olib tashlash uchun start knopkasini bosish
-    driver.find_element(by=By.XPATH, value="//button[@type='button'][@class='btn btn-default btn-close']").click()
-    sleep(2)
-except:
-    pass
-try:
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='userEmail'][@class='form-control userEmail']")))
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='userPassword'][@class='form-control userPassword']")))
+    # login kiritish
+    driver.find_element(by=By.XPATH, value="//input[@id='userEmail'][@class='form-control userEmail']").click()
+    driver.find_element(by=By.XPATH, value="//input[@id='userEmail'][@class='form-control userEmail']").send_keys(email)
+    # parol kiritish
+    driver.find_element(by=By.XPATH, value="//input[@id='userPassword'][@class='form-control userPassword']").click()
+    driver.find_element(by=By.XPATH, value="//input[@id='userPassword'][@class='form-control userPassword']").send_keys(pasword)
+    # login qib kirish knopkasini boish
+    driver.find_element(by=By.XPATH, value="//button[@id='login_btn'][@class='btn btn-theme-primary btn-loader']").click()
+    sleep(10)
+    try:
+        # waiting
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@type='button'][@class='btn btn-default btn-close']")))
+        # Get started oynasini olib tashlash uchun start knopkasini bosish
+        driver.find_element(by=By.XPATH, value="//button[@type='button'][@class='btn btn-default btn-close']").click()
+    except:
+        pass
+    try:
+        # waiting
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@type='button'][@class='close']")))
+        # Verifikatsiya oynasini olib tashlash uchun close knopkasini bosish
+        driver.find_element(by=By.XPATH, value="//button[@type='button'][@class='close']").click()
+    except:
+        pass
     # waiting
-    WebDriverWait(driver, 10)
-    # Verifikatsiya oynasini olib tashlash uchun close knopkasini bosish
-    driver.find_element(by=By.XPATH, value="//button[@type='button'][@class='close']").click()
-    sleep(2)
-except:
-    pass
-# waiting
-WebDriverWait(driver, 10)
-# pasroqga otkazish (mishka ortasidagini aylantirish)
-driver.execute_script("window.scrollTo(0, 400)")
-sleep(3)
-# waiting
-WebDriverWait(driver, 10)
-# change knopkasini bosish
-driver.find_element(by=By.XPATH, value="//button[@type='button'][@Class='btn btn-block btn-theme-secondary change-password']").click()
-WebDriverWait(driver, 10)
-sleep(3)
-# eski parolni kiritish uchun bosish
-driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control']").click()
-driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control']").send_keys("Asdfg_123456")
-WebDriverWait(driver, 10)
-sleep(3)
-# yangi parolni kiritish
-driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_new']").click()
-driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_new']").send_keys("SVkj2dvn12")
-WebDriverWait(driver, 10)
-sleep(3)
-# yangi parolni tasdiqlash uchun kiritish
-driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_confirm']").click()
-driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_confirm']").send_keys("SVkj2dvn12")
-# qoyilgan parolni o'zgartirish
-driver.find_element(by=By.XPATH, value="//button[@id='passwordManageSaveVE'][@class='btn btn-theme-primary btn-tap-save']").click()
-# waiting
-WebDriverWait(driver, 10)
-sleep(4)
-driver.quit()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@type='button'][@class='close']")))
+    # pasroqga otkazish (mishka ortasidagini aylantirish)
+    driver.execute_script("window.scrollTo(0, 400)")
+    # waiting
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@type='button'][@Class='btn btn-block btn-theme-secondary change-password']")))
+    # change knopkasini bosish
+    driver.find_element(by=By.XPATH, value="//button[@type='button'][@Class='btn btn-block btn-theme-secondary change-password']").click()
+    # waiting
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='password'][@class='form-control']")))
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='password'][@class='form-control'][@name='password_new']")))
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='password'][@class='form-control'][@name='password_confirm']")))
+    # eski parolni kiritish
+    driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control']").click()
+    driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control']").send_keys(old_password)
+    # yangi parolni kiritish
+    driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_new']").click()
+    driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_new']").send_keys(new_password)
+    # yangi parolni tasdiqlash
+    driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_confirm']").click()
+    driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_confirm']").send_keys(new_password)
+    # qoyilgan parolni o'zgartirish
+    driver.find_element(by=By.XPATH, value="//button[@id='passwordManageSaveVE'][@class='btn btn-theme-primary btn-tap-save']").click()
+    # waiting
+    sleep(5)
+    driver.quit()
--- test.py
+++ test.py
@@ -1,55 +1,20 @@
-import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from auto_login import Change_password
+import csv
 
-API_TOKEN = '6666656191:AAEtggl0TyzLyPeRggLlI25SDx1dD7izml4'
-GROUP_CHAT_ID = -1002150404034
+email = "siwanac556@qodiq.com"
+pasword = "Asd_1234"
+old_password = "32tk2kgVG"
+new_password = "ASFAGggsdg323"
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
+with open(r'C:\Users\islom\AppData\Roaming\MetaQuotes\Terminal\287469DEA9630EA94D0715D755974F1B\MQL4\Files\find.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        if row:
+            Change_password(email, pasword, old_password, new_password)
 
-# Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
 
-# Handle "hello" message
-@dp.message_handler(lambda message: message.text.lower() == "5226667_daily loss_test test")
-async def handle_hello(message: types.Message):
-    print(123)
-    await message.reply(555)
 
-# Handle all other text messages and store the last message
-@dp.message_handler(content_types=['text'])
-async def handle_text(message: types.Message):
-    global last_text_message
-    last_text_message = message.text
-    await message.reply(f"You said: {message.text}")
 
-# Handle /last command to get the last text message
-@dp.message_handler(commands=['last'])
-async def get_last_message(message: types.Message):
-    global last_text_message
-    if last_text_message:
-        await message.reply(f"The last message you sent was: {last_text_message}")
-    else:
-        await message.reply("No messages received yet.")
 
-# Handle /sendtogroup command to send a message to the group
-@dp.message_handler(commands=['sendtogroup'])
-async def send_to_channel(message: types.Message):
-    try:
-        await bot.send_message(GROUP_CHAT_ID, "This is a message to the group.")
-        await message.reply("Message sent to the group.")
-    except Exception as e:
-        await message.reply(f"Failed to send message to the group: {e}")
 
-# Handle /sendtogroup command to send a message to the group
-
-# Handle all other messages
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.reply(message.text)
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
 
