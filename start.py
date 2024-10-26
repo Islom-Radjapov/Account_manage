5352b58619733eba3c6f61aceb1a2db5c80dcc36
Islom <100282951+Islom-Radjapov@users.noreply.github.com>
2024-10-26 21:48:31 +0500

    Update start.py

--- start.py
+++ start.py
@@ -1,26 +1,41 @@
 from auto_login import Change_password
+from generate_password import generate_password
 from time import sleep
-import csv
+import psutil
 
-email = "yoharo4849@craftapk.com"
+# Close MetaTrader using the process name
+def close_MT5():
+    for proc in psutil.process_iter():
+        try:
+            print(proc)
+            # Check if process name matches MetaTrader
+            if proc.name() == "terminal64.exe":  # This is the name for both MT4 and MT5
+                proc.terminate()  # or proc.kill() for a forced termination
+                print("MetaTrader closed successfully.")
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
+
+
+email = "wofes94415@advitize.com"
 pasword = "Asd_1234"
-old_password = "0a8YDbX-"
-new_password = "ASFAGggsdg323"
+old_password = "ASsdg4e5h_dEDF"
+new_password =  "asasgaggd" #generate_password(11)
+Change_password(email, pasword, old_password, new_password)
 
+
+print("Start checking")
 
 while True:
-    with open(r'C:\Users\islom\AppData\Roaming\MetaQuotes\Terminal\287469DEA9630EA94D0715D755974F1B\MQL4\Files\daily_loss.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in spamreader:
-            if row:
-                print(row)
+    try:
+        with open("\loss.csv", encoding='utf-16') as file:
+            print("file open > ", file.read())
+            print("bool > ", file.read() is not None)
+            if file is not None:
+                print("Change password active")
+                close_MT5()
                 Change_password(email, pasword, old_password, new_password)
-    sleep(3)
-    with open(r'C:\Users\islom\AppData\Roaming\MetaQuotes\Terminal\287469DEA9630EA94D0715D755974F1B\MQL4\Files\max_loss.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in spamreader:
-            if row:
-                print(row)
-                Change_password(email, pasword, old_password, new_password)
+    except:
+        pass
+    sleep(15)
 
-    sleep(30)
