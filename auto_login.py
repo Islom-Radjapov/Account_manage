a6dca942a38a1aa549cf46bc85a46020475cef70
Islom <100282951+Islom-Radjapov@users.noreply.github.com>
2024-10-26 21:43:39 +0500

    Update auto_login.py

--- auto_login.py
+++ auto_login.py
@@ -5,59 +5,71 @@ from time import sleep
 from selenium.webdriver.support import expected_conditions as EC
 
 def Change_password(email, pasword, old_password, new_password):
-    driver = webdriver.Chrome()
-    driver.set_window_size(1550, 800)
-    driver.get("https://global.mytaurex.com/en/account-opening-process?fl=0")
+    try:
+        driver = webdriver.Edge()
+        #driver.set_window_size(1550, 800)
+        driver.get("https://global.mytaurex.com/en/account-opening-process?fl=0")
 
-    # waiting
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='userEmail'][@class='form-control userEmail']")))
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='userPassword'][@class='form-control userPassword']")))
-    # login kiritish
-    driver.find_element(by=By.XPATH, value="//input[@id='userEmail'][@class='form-control userEmail']").click()
-    driver.find_element(by=By.XPATH, value="//input[@id='userEmail'][@class='form-control userEmail']").send_keys(email)
-    # parol kiritish
-    driver.find_element(by=By.XPATH, value="//input[@id='userPassword'][@class='form-control userPassword']").click()
-    driver.find_element(by=By.XPATH, value="//input[@id='userPassword'][@class='form-control userPassword']").send_keys(pasword)
-    # login qib kirish knopkasini boish
-    driver.find_element(by=By.XPATH, value="//button[@id='login_btn'][@class='btn btn-theme-primary btn-loader']").click()
-    sleep(10)
-    try:
         # waiting
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@type='button'][@class='btn btn-default btn-close']")))
-        # Get started oynasini olib tashlash uchun start knopkasini bosish
-        driver.find_element(by=By.XPATH, value="//button[@type='button'][@class='btn btn-default btn-close']").click()
-    except:
-        pass
-    try:
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='userEmail'][@class='form-control userEmail']")))
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='userPassword'][@class='form-control userPassword']")))
+        # login kiritish
+        driver.find_element(by=By.XPATH, value="//input[@id='userEmail'][@class='form-control userEmail']").click()
+        driver.find_element(by=By.XPATH, value="//input[@id='userEmail'][@class='form-control userEmail']").send_keys(email)
+        # parol kiritish
+        driver.find_element(by=By.XPATH, value="//input[@id='userPassword'][@class='form-control userPassword']").click()
+        driver.find_element(by=By.XPATH, value="//input[@id='userPassword'][@class='form-control userPassword']").send_keys(pasword)
+        # login qib kirish knopkasini boish
+        driver.find_element(by=By.XPATH, value="//button[@id='login_btn'][@class='btn btn-theme-primary btn-loader']").click()
+        sleep(7)
+        try:
+            # waiting
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@type='button'][@class='btn btn-default btn-close']")))
+            # Get started oynasini olib tashlash uchun start knopkasini bosish
+            driver.find_element(by=By.XPATH, value="//button[@type='button'][@class='btn btn-default btn-close']").click()
+        except:
+            pass
+        try:
+            # waiting
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@type='button'][@class='close']")))
+            # Verifikatsiya oynasini olib tashlash uchun close knopkasini bosish
+            driver.find_element(by=By.XPATH, value="//button[@type='button'][@class='close']").click()
+        except:
+            pass
+        #sleep(3)
         # waiting
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@type='button'][@class='close']")))
-        # Verifikatsiya oynasini olib tashlash uchun close knopkasini bosish
-        driver.find_element(by=By.XPATH, value="//button[@type='button'][@class='close']").click()
-    except:
-        pass
-    # waiting
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@type='button'][@class='close']")))
-    # pasroqga otkazish (mishka ortasidagini aylantirish)
-    driver.execute_script("window.scrollTo(0, 400)")
-    # waiting
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@type='button'][@Class='btn btn-block btn-theme-secondary change-password']")))
-    # change knopkasini bosish
-    driver.find_element(by=By.XPATH, value="//button[@type='button'][@Class='btn btn-block btn-theme-secondary change-password']").click()
-    # waiting
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='password'][@class='form-control']")))
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='password'][@class='form-control'][@name='password_new']")))
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='password'][@class='form-control'][@name='password_confirm']")))
-    # eski parolni kiritish
-    driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control']").click()
-    driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control']").send_keys(old_password)
-    # yangi parolni kiritish
-    driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_new']").click()
-    driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_new']").send_keys(new_password)
-    # yangi parolni tasdiqlash
-    driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_confirm']").click()
-    driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_confirm']").send_keys(new_password)
-    # qoyilgan parolni o'zgartirish
-    driver.find_element(by=By.XPATH, value="//button[@id='passwordManageSaveVE'][@class='btn btn-theme-primary btn-tap-save']").click()
-    # waiting
-    sleep(5)
-    driver.quit()
+        # pasroqga otkazish (mishka ortasidagini aylantirish)
+        driver.execute_script("window.scrollTo(0, 400)")
+        # waiting
+        WebDriverWait(driver, 10).until(EC.invisibility_of_element((By.CLASS_NAME, "modal-backdrop")))
+        # change knopkasini bosish
+        btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@type='button'][@Class='btn btn-block btn-theme-secondary change-password']")))
+        btn.click()
+        # change knopkasini bosish id orqali iminoy tanlab bosish
+        #WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@data-trade_platform_id='810004304' and @data-account_id='33900' and @data-at-id='3']"))).click()
+        # waiting
+        #sleep(5)
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='password'][@class='form-control']")))
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='password'][@class='form-control'][@name='password_new']")))
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@type='password'][@class='form-control'][@name='password_confirm']")))
+        # eski parolni kiritish
+        driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control']").click()
+        driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control']").send_keys(old_password)
+        # yangi parolni kiritishi ined
+        driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_new']").click()
+        driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_new']").send_keys(new_password)
+        # yangi parolni tasdiqlash
+        driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_confirm']").click()
+        driver.find_element(by=By.XPATH, value="//input[@type='password'][@class='form-control'][@name='password_confirm']").send_keys(new_password)
+        # qoyilgan parolni o'zgartirish
+        driver.find_element(by=By.XPATH, value="//button[@id='passwordManageSaveVE'][@class='btn btn-theme-primary btn-tap-save']").click()
+
+        # waiting
+        sleep(7)
+        driver.quit()
+    except Exception as error:
+        try:
+            print("ERROR try => ", error)
+            driver.quit()
+        except:
+            pass
