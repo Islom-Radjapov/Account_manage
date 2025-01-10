7e29ff18e44c3b390064f884144957a6f5d43f9f
Islom <100282951+Islom-Radjapov@users.noreply.github.com>
2025-01-10 22:35:42 +0500

    Update start.py

--- start.py
+++ start.py
@@ -1,41 +1,40 @@
-from auto_login import Change_password
-from generate_password import generate_password
-from time import sleep
-import psutil
+import MetaTrader5 as mt5
+from _datetime import datetime
+login = 576500
+password = "HzS-Dz2o"
+server = 'BlueWhaleMarkets-Server'
 
-# Close MetaTrader using the process name
-def close_MT5():
-    for proc in psutil.process_iter():
-        try:
-            print(proc)
-            # Check if process name matches MetaTrader
-            if proc.name() == "terminal64.exe":  # This is the name for both MT4 and MT5
-                proc.terminate()  # or proc.kill() for a forced termination
-                print("MetaTrader closed successfully.")
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
+initialize = mt5.initialize()
+mt5.login(login=login, password=password, server=server)
+if initialize:
+    print('Connected to MetaTrader5')
+    print('Login=>', mt5.account_info().login)
+    print('Balance=>', mt5.account_info().balance)
+    print('Server=>', mt5.account_info().server)
 
 
+    while True:
+        positions = mt5.positions_get(symbol="EURUSDp")
+        print("Account Size ", positions[0])
+        close_request = {
+            "action": mt5.TRADE_ACTION_DEAL,
+            "symbol": "EURUSDp",
+            "volume": 1.0,
+            "type": 1,
+            "position": positions[0].ticket,
+            "price": mt5.symbol_info_tick("EURUSDp").bid,
+            "deviation": 10,
+            "magic": 0,
+            "comment": "",
+            "type_time": mt5.ORDER_TIME_GTC,  # good till cancelled
+            "type_filling": mt5.ORDER_FILLING_RETURN,
+        }
+        # send a close request
+        result = mt5.order_send(close_request)
+        #tick = mt5.symbol_info_tick("EURUSDp").bid
+        print("result", result, "    ", mt5.last_error())
+        break
 
-email = "wofes94415@advitize.com"
-pasword = "Asd_1234"
-old_password = "ASsdg4e5h_dEDF"
-new_password =  "asasgaggd" #generate_password(11)
-Change_password(email, pasword, old_password, new_password)
 
-
-print("Start checking")
-
-while True:
-    try:
-        with open("\loss.csv", encoding='utf-16') as file:
-            print("file open > ", file.read())
-            print("bool > ", file.read() is not None)
-            if file is not None:
-                print("Change password active")
-                close_MT5()
-                Change_password(email, pasword, old_password, new_password)
-    except:
-        pass
-    sleep(15)
-
+else:
+    print("NO Connected")
